eb595f932f097bd6ccbb3cd4fd09fb626ae7d450
George Downer <george.downer40@gmail.com>
2024-07-04 10:56:39 +0000

    configure secret key

--- legomanager/settings.py
+++ legomanager/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pf!z+7s&_$--#(*@r&@do)hpu=mw%67g3+q2&axal2jp9tb$8)'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
